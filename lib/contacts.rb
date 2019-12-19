f3aa09ec1a977ef3c17444b4d80a53b6b4ef8217
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2019-12-19 21:06:29 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -11,7 +11,7 @@ require 'pry'
      # name: "Freddy",
    #   email: "freddy@mercury.com",
   #    favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
-    }
+   # }
   #    }
    # end
   
