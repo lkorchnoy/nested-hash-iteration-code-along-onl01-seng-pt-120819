9673f30f94014257b2119eec1a60ebbc931a9e31
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2019-12-19 21:13:07 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,5 +19,16 @@ def remove_strawberry(contacts)
 contacts.each do |person, contact_details_hash|
  binding.pry
 end
-
+contacts = {
+    "Jon Snow" => {
+     name: "Jon",
+      email: "jon_snow@thewall.we", 
+     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+    },
+   "Freddy Mercury" => {
+     name: "Freddy",
+     email: "freddy@mercury.com",
+    favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+   }
+    }
 end
